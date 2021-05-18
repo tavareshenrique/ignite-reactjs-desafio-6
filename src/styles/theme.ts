8fbc49b340b280b5c9eef706b154ffab2916e880
Henrique Tavares <ihenrits@gmail.com>
2021-05-18 07:47:38 -0300

    add chakra-ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '700': '#47585B',
+      '50': '#F5F8Fa',
+    },
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        color: 'gray.700',
+      },
+      h1: {
+        fontSize: '4xl',
+        fontWeight: '700',
+        mb: '4',
+      },
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        cursor: 'pointer',
+      },
+    },
+  },
+});
